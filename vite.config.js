569eea95b04a2c2411e9d293bdd1c27d9c6714be
ToroTu <tu8247@gmail.com>
2023-06-04 23:56:50 +0800

    20230604 update vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        // chunkFileNames: 'assets/[name]-11-[hash].js',
+        // assetFileNames: 'assets/[name]-22-[hash].[ext]',
+        // entryFileNames: 'assets/[name]-33-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (/.+\.css$/.test(assetInfo.name)) {
+            return 'assets/style.[ext]'
+          }
+          return 'assets/[name]-[hash].[ext]'
+        },
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
 })
